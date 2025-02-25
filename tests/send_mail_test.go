c4f1fdbc4966a05e75e382414ac57cd551ed3177
[Anurag Raut] <rautab0@gmail.com>
2025-02-25 18:53:52 +0530

    fix:sendEmail test

--- tests/send_mail_test.go
+++ tests/send_mail_test.go
@@ -1,22 +1,47 @@
 package tests
 
 import (
-	"bytes"
+	//"bytes"
 	"fmt"
+	"io"
 	"testing"
 
-	"github.com/emersion/go-sasl"
+	//"github.com/emersion/go-sasl"
 	"github.com/emersion/go-smtp"
 )
 
 func sendMail(to, subject, body string) error {
+
 	from := "rautab0@gmail.com"
 	smtpServer := "localhost:1025"
-	reader := bytes.NewReader([]byte(body))
-	err := smtp.SendMail(smtpServer, sasl.NewPlainClient("", "", ""), from, []string{to}, reader)
+	c, err := smtp.Dial(smtpServer)
 	if err != nil {
 		return err
 	}
+	if err := c.Mail(from, nil); err != nil {
+		return err
+	}
+	if err := c.Rcpt(to, nil); err != nil {
+		return err
+	}
+	wc, err := c.Data()
+	if err != nil {
+		return err
+	}
+	_, err = io.WriteString(wc, body)
+	if err != nil {
+		return nil
+	}
+
+	err = wc.Close()
+	if err != nil {
+		return nil
+	}
+	err = c.Quit()
+	if err != nil {
+		return nil
+	}
+
 	return nil
 }
 
